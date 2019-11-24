c505f241f79f35ba9fdde92d13deb13091e3a769
our ame <you@example.com>
2019-11-24 18:56:54 +0700

    Добавил выделение по клику

--- src/components/CardResult.tsx
+++ src/components/CardResult.tsx
@@ -13,20 +13,44 @@ const CardResult = tsx.componentFactoryOf<IEvents>().create({
         code: { type: String },
     },
 
+    data: () => ({
+        select: true
+    }),
+
     methods: {
+        selectAll() {
+            if (!this.select) return
+            let target = document.getElementById('ex5');
+            if (document.createRange) {
+                let rng = document.createRange();
+                rng.selectNode(target)
+                let sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(rng);
+            } else {
+                var rng = document.body.createTextRange();
+                rng.moveToElementText(target);
+                rng.select();
+            }
+        }
     },
 
     mounted() {
     },
 
     render(): VNode {
-        return (
-            <div class="card">
-                <div class="card-body">
-                    {formatter.render(this.code)}
-                </div>
+        return <div class="card">
+            <div class="card-header">
+                <label>
+                    <input type="checkbox" checked v-model={this.select} />
+                    &nbsp; Выбиратьпо клику
+                </label>
             </div>
-        )
+            <div class="card-body" id="ex5" onClick={this.selectAll}>
+                {formatter.render(this.code)}
+            </div>
+        </div>
+
     }
 
 })

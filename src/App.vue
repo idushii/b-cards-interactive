a92cadd1835ff7dbd77c326d396a8adc8b6f0a59
our ame <you@example.com>
2019-11-24 15:18:29 +0700

    Заготовка card

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <h1 class="display-1">Интерактивный пример card bootstrap</h1>
+    <Card />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Card from './components/Card.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Card,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
